ae31a60187499e7177dedd27971374613917ff83
electro2op <140671366+electro2op@users.noreply.github.com>
2023-10-27 15:47:53 +0530

    Update urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   
     path('',views.index),
     # path('details/',views.details,name='details'),
     path('delete/<int:taskid>/', views.delete, name='delete'),
